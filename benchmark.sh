8252faef50ec9da0a565ba4f079da3f593e94ea3
BangJagu <76428883+BangJagu@users.noreply.github.com>
2020-12-26 16:35:55 +0800

    Add files via upload

---
+++ benchmark.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#vps="zvur";
+vps="aneka";
+
+if [[ $vps = "zvur" ]]; then
+	source="http://scripts.gapaiasa.com"
+else
+	source="https://raw.githubusercontent.com/r38865/VPS/master/Update"
+fi
+
+# go to root
+cd
+
+wget $source/bench.sh -O - -o /dev/null|bash
